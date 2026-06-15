2182ba8b8cb856feb3f75a160a97675d47ec9869
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 00:54:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-
 const http = require('http');
+const fs = require('fs');
+
+console.log("Naveed MD Loading...");
 
 http.createServer((req, res) => {
-  res.end("Naveed MD Stable Server");
+res.end("Naveed MD Online ✅");
 }).listen(process.env.PORT || 3000);
 
+const commands = fs.readdirSync('./commands');
+console.log("Commands Loaded:", commands.length);
+
 console.log("Running OK");
-console.log("Naveed MD Loading...");
