48242548be61e32b8691792fd99231eed70f4883
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 18:00:26 +0500

    Create credits.js

---
+++ credits.js
@@ -0,0 +1,15 @@
+module.exports = {
+name: "credits",
+run: async () => {
+return `
+🎖 NAVEED MD CREDITS
+
+👑 Owner: Naveed
+🤖 Bot: Naveed MD
+⚡ Powered By: Baileys
+🇵🇰 Pakistan
+
+Thank You For Using Naveed MD ❤️
+`;
+}
+}
