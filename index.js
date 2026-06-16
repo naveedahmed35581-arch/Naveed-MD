8279f8e51fd49e7f7227f7fd5b43c6fcb769b959
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 11:56:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
-const fs = require("fs");
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys");
 const http = require("http");
 
 console.log("Naveed MD Starting...");
 
 async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState('./session');
+const { state, saveCreds } = await useMultiFileAuthState("./session");
 
-    const sock = makeWASocket({
-        auth: state,
-        printQRInTerminal: true
-    });
+const sock = makeWASocket({
+    auth: state,
+    browser: ["Naveed MD", "Chrome", "1.0.0"]
+});
 
-    sock.ev.on("creds.update", saveCreds);
+sock.ev.on("creds.update", saveCreds);
 
-    sock.ev.on("connection.update", (update) => {
-        const { connection } = update;
+sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
 
-        if (connection === "open") {
-            console.log("✅ WhatsApp Connected!");
-        }
+    if (connection === "open") {
+        console.log("✅ WhatsApp Connected!");
+    }
 
-        if (connection === "close") {
-            console.log("❌ Disconnected, restarting...");
+    if (connection === "close") {
+        console.log("❌ Connection Closed");
+
+        const shouldReconnect =
+            lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
+
+        if (shouldReconnect) {
             startBot();
         }
-    });
+    }
+});
+
 }
 
 startBot();
 
 http.createServer((req, res) => {
-    res.end("Naveed MD Bot Running");
+res.end("Naveed MD Running");
 }).listen(process.env.PORT || 3000);
 
 console.log("Server Running OK");
