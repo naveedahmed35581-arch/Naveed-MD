3b4ddb3314d2e6e7753b46f9752e1943b0b1e8e2
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 21:15:47 +0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,37 +1,68 @@
 module.exports = async (sock, m) => {
-    try {
+try {
 
-        const msg = m.messages[0];
-        if (!msg.message) return;
+    const msg = m.messages[0];
+    if (!msg.message) return;
 
-        const text =
-            msg.message.conversation ||
-            msg.message.extendedTextMessage?.text || "";
+    const text =
+        msg.message.conversation ||
+        msg.message.extendedTextMessage?.text || "";
 
-        const prefix = ".";
+    const prefix = ".";
 
-        // simple commands
-        if (text === prefix + "ping") {
-            await sock.sendMessage(msg.key.remoteJid, { text: "Pong 🏓" });
-        }
-
-        if (text === prefix + "alive") {
-            await sock.sendMessage(msg.key.remoteJid, { text: "Naveed MD is Alive ✅" });
-        }
-
-        if (text === prefix + "menu") {
-            await sock.sendMessage(msg.key.remoteJid, {
-                text: "╔═══『 NAVEED MD 』\n║ .ping\n║ .alive\n║ .menu\n╚═══════════"
-            });
-        }
-
-        if (text === prefix + "info") {
-            await sock.sendMessage(msg.key.remoteJid, {
-                text: `🤖 Bot: Naveed MD\n👑 Owner: Naveed\n⚡ Status: Online`
-            });
-        }
-
-    } catch (e) {
-        console.log("Handler Error:", e);
+    if (text === prefix + "ping") {
+        await sock.sendMessage(msg.key.remoteJid, {
+            text: "Pong 🏓"
+        });
     }
+
+    if (text === prefix + "alive") {
+        await sock.sendMessage(msg.key.remoteJid, {
+            text: "Naveed MD is Alive ✅"
+        });
+    }
+
+    if (text === prefix + "menu") {
+        await sock.sendMessage(msg.key.remoteJid, {
+            image: {
+                url: "https://bandaheali-cdn.koyeb.app/media/bot_1781626177072.jpeg"
+            },
+            caption: `
+
+╔════════════════════╗
+║ 🤖 NAVEED MD
+╚════════════════════╝
+
+👑 OWNER
+• .owner
+• .support
+
+⚡ BOT
+• .ping
+• .alive
+• .info
+• .menu
+
+📢 CHANNEL
+https://whatsapp.com/channel/0029Vawbj7e5kg7AFF5MuZ28
+
+👑 OWNER
+923213231887
+
+🌍 MODE : PUBLIC
+⚡ VERSION : 1.0.0
+`
+});
+}
+
+    if (text === prefix + "info") {
+        await sock.sendMessage(msg.key.remoteJid, {
+            text: `🤖 Bot: Naveed MD\n👑 Owner: Naveed\n⚡ Status: Online`
+        });
+    }
+
+} catch (e) {
+    console.log("Handler Error:", e);
+}
+
 };
