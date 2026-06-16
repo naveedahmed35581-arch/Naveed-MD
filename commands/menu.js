b838a80e799eace51e29bdc718df84b6832f07f8
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-06-16 20:25:22 +0500

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,24 +1,45 @@
 module.exports = {
-  name: "menu",
-  run: async () => {
-    return `
-╔══════════════╗
+name: "menu",
+run: async () => {
+return `
+╔════════════════════╗
 ║ 🤖 NAVEED MD
-╚══════════════╝
+╚════════════════════╝
 
-👑 OWNER COMMANDS
+👑 OWNER
 • .owner
+• .support
 
-⚡ BOT COMMANDS
+⚡ BOT
 • .ping
 • .alive
 • .info
+• .status
+• .help
 • .menu
 
-╔══════════════╗
+📊 SYSTEM
+• .uptime
+• .runtime
+• .version
+
+📚 INFO
+• .about
+• .rules
+• .repo
+• .credits
+
+📢 CHANNEL
+https://whatsapp.com/channel/0029Vawbj7e5kg7AFF5MuZ28
+
+👑 OWNER NUMBER
+923213231887
+
+╔════════════════════╗
 ║ Status : Online ✅
-║ Version : 1.0
-╚══════════════╝
+║ Version : 1.0.0
+║ Mode : Public 🌍
+╚════════════════════╝
 `;
-  }
+}
 }
