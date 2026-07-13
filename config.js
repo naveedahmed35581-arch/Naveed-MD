c28fa19b7964bc65d1c74e27610537e48649be7e
naveedahmed35581-arch <naveedahmed.35581@gmail.com>
2026-07-13 11:34:34 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-BOT_NAME: "Naveed MD",
-OWNER_NAME: "Naveed",
-OWNER_NUMBER: "923213231887",
-CHANNEL_LINK: "https://whatsapp.com/channel/0029Vawbj7e5kg7AFF5MuZ28",
-PREFIX: "."
-}
